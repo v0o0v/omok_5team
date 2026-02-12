fcc298bc48e2473aca00a64c47251cea72339505
ionic5 <st.kaldalis@gmail.com>
2026-02-12 15:59:00 +0900

    Time 추가

---
+++ Assets/Scripts/Common/Time.cs
@@ -0,0 +1,29 @@
+﻿using Omok;
+using System;
+
+namespace Omok
+{
+    public class Time : ITime
+    {
+        public long GetCurrentTimeMilliseconds()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+
+        public long GetCurrentTime()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+
+        public float GetDeltaTime()
+        {
+            return UnityEngine.Time.deltaTime;
+        }
+
+        public long GetToday()
+        {
+            DateTime todayUtc = DateTime.UtcNow.Date;
+            return new DateTimeOffset(todayUtc).ToUnixTimeSeconds();
+        }
+    }
+}
