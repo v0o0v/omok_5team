3ab0f270ec21d83da6abcca395e1e5b52c627ece
leomanic <croasoap@gmail.com>
2026-02-12 20:48:26 +0900

    사운드 효과 추가

---
+++ Assets/Scripts/Common/SoundManager.cs
@@ -0,0 +1,100 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+// 배경음악(BGM)
+public enum Enum_Bgm
+{
+    TITLE,
+    GAME_BACKGOUND,
+    RESULT,
+}
+
+// 효과음(SFX)
+public enum Enum_Sfx
+{
+    FALL1,
+    FALL2,
+    PLACE_STONE1,
+    PLACE_STONE2,
+    PLACE_STONE3,
+    WINNING1,
+    WINNING2,
+}
+
+public class SoundManager : MonoBehaviour
+{
+public static SoundManager instance;
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip[] bgmClips;  // BGM 클립 배열
+    [SerializeField] private AudioClip[] sfxClips; // SFX 클립 배열
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource bgmSource; // BGM 재생 AudioSource
+    [SerializeField] private AudioSource sfxSource; // SFX 재생 AudioSource
+
+    private Dictionary<Enum_Bgm, AudioClip> bgmDict; // BGM Dictionary
+    private Dictionary<Enum_Sfx, AudioClip> sfxDict; // SFX Dictionary
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        InitDictionaries();
+    }
+
+    // Dictionary 초기화
+    private void InitDictionaries()
+    {
+        bgmDict = new Dictionary<Enum_Bgm, AudioClip>();
+        for (int i = 0; i < bgmClips.Length; i++)
+        {
+            bgmDict[(Enum_Bgm)i] = bgmClips[i];
+        }
+
+        sfxDict = new Dictionary<Enum_Sfx, AudioClip>();
+        for (int i = 0; i < sfxClips.Length; i++)
+        {
+            sfxDict[(Enum_Sfx)i] = sfxClips[i];
+        }
+    }
+
+    // BGM 재생
+    public void PlayBGM(Enum_Bgm bgmType)
+    {
+        if (bgmDict.TryGetValue(bgmType, out var clip))
+        {
+            bgmSource.clip = clip;
+            bgmSource.loop = true; // 배경음악은 기본적으로 반복 재생
+            bgmSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("BGM not found in Dictionary!");
+        }
+    }
+
+    // SFX 재생
+    public void PlaySFX(Enum_Sfx sfxType)
+    {
+        if (sfxDict.TryGetValue(sfxType, out var clip))
+        {
+            sfxSource.PlayOneShot(clip);
+            Debug.LogWarning("SFX found in Dictionary!");
+        }
+        else
+        {
+            Debug.LogWarning("SFX not found in Dictionary!");
+        }
+    }
+}
--- Assets/Scripts/Game/Block.cs
+++ Assets/Scripts/Game/Block.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using UnityEngine;
 
 namespace Omok {
@@ -8,20 +8,24 @@ namespace Omok {
         [SerializeField] private Sprite blackStoneSprite;
         [SerializeField] private Sprite whiteStoneSprite;
         private SpriteRenderer spriteRenderer;
-
         private int _x;
         private int _y;
 
         private Action<int, int> _onBlockClicked;
+        // 사운드 매니저 추가 - [leomanic]
+        private SoundManager sound;
 
         private void Awake(){
             spriteRenderer = GetComponent<SpriteRenderer>();
+            sound = FindFirstObjectByType<SoundManager>();
         }
 
         public void InitMarker(int x, int y, Constants.PlayerType playerType){
             _x = x;
             _y = y;
             SetMarker(playerType);
+            Debug.Log("플레이 사운드");
+            SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE2);
         }
 
         public void SetMarker(Constants.PlayerType markerType){
