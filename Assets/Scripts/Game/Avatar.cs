1b7d011f0c2d82e8ebfdd779f1d9076a16121dd9
ionic5 <st.kaldalis@gmail.com>
2026-02-13 10:14:31 +0900

    아바타 연출 추가

--- Assets/Scripts/Game/Avatar.cs
+++ Assets/Scripts/Game/Avatar.cs
@@ -1,15 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game
 {
     public class Avatar : MonoBehaviour
     {
-        //[SerializeField]
-        //private Animator _animator;
+        [SerializeField]
+        private Sprite _avatarWin;
+        [SerializeField]
+        private Sprite _avatarLose;
+        [SerializeField]
+        private Sprite _avatarThink;
+        [SerializeField]
+        private Sprite _avatarWait;
+
+        [SerializeField]
+        private Image _avatarImage;
+
+        private Dictionary<string, Sprite> _avatarSprites;
+
+        private void Awake()
+        {
+            _avatarSprites = new Dictionary<string, Sprite>();
+            _avatarSprites.Add(AvatarAnimationID.Wait, _avatarWait);
+            _avatarSprites.Add(AvatarAnimationID.Win, _avatarWin);
+            _avatarSprites.Add(AvatarAnimationID.Lose, _avatarLose);
+            _avatarSprites.Add(AvatarAnimationID.Think, _avatarThink);
+        }
 
         public void PlayAnimation(string animationID)
         {
-            //_animator.Play(animationID);
+            if (_avatarSprites.TryGetValue(animationID, out var sprite))
+                _avatarImage.sprite = sprite;
         }
     }
 }
