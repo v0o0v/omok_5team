bc0da716f2d2b693c6078f079b9e3341aefbdc8e
leomanic <croasoap@gmail.com>
2026-02-24 17:34:45 +0900

    종료 팝업시 기능 추가(계속진행 버튼)
    로직은 아직 연결 전

--- Assets/Scripts/UI/ConfirmPanelController.cs
+++ Assets/Scripts/UI/ConfirmPanelController.cs
@@ -7,16 +7,28 @@ namespace Omok {
     public class ConfirmPanelController : PanelController {
 
         [SerializeField] private TMP_Text messageText;
+        [SerializeField] private GameObject buttonLaytout;
         private Action _onConfirmButtonClicked;
+        private Action _onContinueButtonClicked;
 
         public void Show(string message, Action onConfirmButtonClick = null){
             this._onConfirmButtonClicked = onConfirmButtonClick;
             messageText.text = message;
             base.Show();
         }
+        public void Show(string message, Action onConfirmButtonClick = null, Action onContinueButtonClick = null){
+            this._onConfirmButtonClicked = onConfirmButtonClick;
+            this._onContinueButtonClicked = onContinueButtonClick;
+            buttonLaytout.SetActive(true);
+            messageText.text = message;
+            base.Show();
+        }
 
         public void OnClickCloseButton(){
             Hide(() => { _onConfirmButtonClicked?.Invoke(); });
+        }
+        public void OnClickContinueButton(){
+            Hide(() => { _onContinueButtonClicked?.Invoke(); });
         }
 
     }
