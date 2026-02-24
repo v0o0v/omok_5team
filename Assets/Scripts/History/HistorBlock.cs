16e8c3afd01cb61c6ec44dfbb2baf7cb33bb31ab
김민석 <v0o0v@daum.net>
2026-02-24 15:20:59 +0900

    [기보보기] 기보 로딩

---
+++ Assets/Scripts/History/HistorBlock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using static Omok.Constants;
+
+namespace Omok
+{
+
+    public class HistoryBlock : MonoBehaviour
+    {
+
+        [SerializeField] private Sprite blackStoneSprite;
+        [SerializeField] private Sprite whiteStoneSprite;
+        [SerializeField] TextMeshProUGUI countText;
+        private SpriteRenderer spriteRenderer;
+        private int _x;
+        private int _y;
+
+        private Action<int, int> _onBlockClicked;
+        private SoundManager sound;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            sound = FindFirstObjectByType<SoundManager>();
+        }
+
+        public void InitMarker(int x, int y, PlayerType playerType, int count)
+        {
+            _x = x;
+            _y = y;
+            SetMarker(playerType);
+            // SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE2);
+            countText.text = count.ToString();
+            countText.color = playerType == PlayerType.Player1 ? Color.white : Color.black;
+        }
+
+        public void SetMarker(PlayerType markerType)
+        {
+            switch (markerType)
+            {
+                case PlayerType.Player1:
+                    spriteRenderer.sprite = blackStoneSprite;
+                    break;
+                case PlayerType.Player2:
+                    spriteRenderer.sprite = whiteStoneSprite;
+                    break;
+                case PlayerType.None:
+                    spriteRenderer.sprite = null;
+                    break;
+            }
+        }
+
+    }
+
+}
--- Assets/Scripts/History/HistoryManager.cs
+++ Assets/Scripts/History/HistoryManager.cs
@@ -13,16 +13,16 @@ namespace Omok
 
         public static void HistorySave(BaseState player1, BaseState player2, string result, List<Move> moves)
         {
-            var now = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+            var now = DateTime.Now;
             HistorySheet historySheet = new HistorySheet(
                 player1: player1 is PlayerState ? "Human" : "AI",
                 player2: player2 is PlayerState ? "Human" : "AI",
                 result: result,
-                endDateTime: now,
+                dateTime: now,
                 moves: moves
             );
 
-            Save(historySheet, $"save_{now}.json");
+            Save(historySheet, $"history_{now.ToString("yyyy_MM_dd_HH_mm_ss")}.json");
         }
 
         public static void Save<T>(T data, string fileName)
@@ -48,6 +48,20 @@ namespace Omok
             string json = File.ReadAllText(path);
             return JsonConvert.DeserializeObject<T>(json);
         }
+        
+        public static List<string> GetHistoryFiles()
+        {
+            string dir = Path.Combine(Application.persistentDataPath, "history");
+            if (!Directory.Exists(dir)) return new List<string>();
+
+            string[] files = Directory.GetFiles(dir, "history_*.json");
+            List<string> fileNames = new List<string>();
+            foreach (string file in files)
+            {
+                fileNames.Add(Path.GetFileName(file));
+            }
+            return fileNames;
+        }
 
     }
 
@@ -57,15 +71,15 @@ namespace Omok
         public string player1;
         public string player2;
         public string result;
-        public string endDateTime;
+        public DateTime dateTime;
         public List<Move> moves;
 
-        public HistorySheet(string player1, string player2, string result, string endDateTime, List<Move> moves)
+        public HistorySheet(string player1, string player2, string result, DateTime dateTime, List<Move> moves)
         {
             this.player1 = player1;
             this.player2 = player2;
             this.result = result;
-            this.endDateTime = endDateTime;
+            this.dateTime = dateTime;
             this.moves = moves;
         }
 
