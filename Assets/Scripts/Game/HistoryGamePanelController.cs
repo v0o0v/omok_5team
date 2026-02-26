70817bc9b186f3a55b1483b7359cc4778f90e54e
김민석 <v0o0v@daum.net>
2026-02-26 17:26:39 +0900

    [기보보기] 기보보기용 GamePanelController 추가

---
+++ Assets/Scripts/Game/HistoryGamePanelController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Omok
+{
+
+    public class HistoryGamePanelController : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            SoundManager.instance.PlayBGM(Enum_Bgm.GAME_BACKGOUND);
+        }
+
+        public void OnClickBackButton()
+        {
+            GameManager.Instance.OpenConfirmPanel("기보 보기를 종료합니다", () => { GameManager.Instance.ChangeToMainScene(); });
+        }
+
+        public void OnClickSettingsButton()
+        {
+            GameManager.Instance.OpenSettingsPanel();
+        }
+
+    }
+
+}
