c6aec7a2a77c49f101ddf316938b468698363d7d
leomanic <croasoap@gmail.com>
2026-02-26 22:08:56 +0900

    메인화면 사운드매니저 추가
    메인화면 배경음, 효과음 적용

--- Assets/Scripts/Main/MainPanelController.cs
+++ Assets/Scripts/Main/MainPanelController.cs
@@ -5,23 +5,32 @@ namespace Omok {
 
     public class MainPanelController : MonoBehaviour {
 
+        void Start()
+        {
+            SoundManager.instance.PlayBGM(Enum_Bgm.TITLE);
+        }
+
         // 싱글 플레이
         public void onClickSinglePlayButton(){
+            SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE3);
             GameManager.Instance.ChangeToGameScene(Constants.GameType.SinglePlay);
         }
 
         // 2인 플레이
         public void onClickDualPlayButton(){
+            SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE3);
             GameManager.Instance.ChangeToGameScene(Constants.GameType.DualPlay);
         }
 
         // 기보 보기
         public void onClickGameRecordButton(){
+            SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE3);
             GameManager.Instance.ChangeToHistory();
         }
 
         // 설정 팝업
         public void onClickSettingsButton(){
+            SoundManager.instance.PlaySFX(Enum_Sfx.PLACE_STONE3);
             GameManager.Instance.OpenSettingsPanel();
         }
     }
