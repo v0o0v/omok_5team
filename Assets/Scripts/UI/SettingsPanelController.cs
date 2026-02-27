e24e7d002b44d497af2999a8a99e7edbe184fff9
ionic5 <st.kaldalis@gmail.com>
2026-02-27 13:40:54 +0900

    설정 창 토글 부분 구현

--- Assets/Scripts/UI/SettingsPanelController.cs
+++ Assets/Scripts/UI/SettingsPanelController.cs
@@ -1,12 +1,37 @@
-﻿namespace Omok {
+﻿using Common;
+using UnityEngine;
 
-    public class SettingsPanelController : PanelController {
+namespace Omok
+{
+    public class SettingsPanelController : PanelController
+    {
+        private SoundManager _soundManager;
 
-     public void OnClickCloseButton(){   
-         
-         base.Hide();
-     }
+        [SerializeField]
+        public SwitchToggle _sfxSwitchToggle;
+        [SerializeField]
+        public SwitchToggle _bgmSwitchToggle;
 
+        private void Start()
+        {
+            _soundManager = SoundManager.instance;
+            _sfxSwitchToggle.isOn = _soundManager.IsSFXEnabled();
+            _bgmSwitchToggle.isOn = _soundManager.IsBGMEnabled();
+        }
+
+        public void OnClickCloseButton()
+        {
+            base.Hide();
+        }
+
+        public void OnBGMToggleChanged(bool value)
+        {
+            _soundManager.EnableBGM(value);
+        }
+
+        public void OnSFXToggleChanged(bool value)
+        {
+            _soundManager.EnableSFX(value);
+        }
     }
-
 }
